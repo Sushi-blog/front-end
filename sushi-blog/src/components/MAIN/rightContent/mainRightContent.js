74a1387435c881a01f36c13cc850e4b87c0b820a
Kimjimin0807 <jm004471@naver.com>
2021-03-05 23:32:07 +0900

    ADD: 메인 퍼블리싱 코드 추가

--- sushi-blog/src/components/MAIN/rightContent/mainRightContent.js
+++ sushi-blog/src/components/MAIN/rightContent/mainRightContent.js
@@ -4,13 +4,42 @@ import * as s from './style';
 const MainRightContent = () => {
     return (
         <>
-            <s.PostContainer>
+            <s.MainContainer>
                 <s.ExplanaitionDiv>
                     <p>카테고리</p>
                     <p>제목</p>
                     <p>작성일</p>
                 </s.ExplanaitionDiv>
-            </s.PostContainer>
+                <s.PostContainer>
+                    <s.Category>[카테고리1]</s.Category>
+                    <s.Title>이승윤 오늘은 디자인을 하다</s.Title>
+                    <s.CreateDate>2021-02-23</s.CreateDate>
+                </s.PostContainer>
+                <s.PostContainer>
+                    <s.Category>[카테고리1]</s.Category>
+                    <s.Title>이승윤 오늘은 디자인을 하다</s.Title>
+                    <s.CreateDate>2021-02-23</s.CreateDate>
+                </s.PostContainer>
+                <s.PostContainer>
+                    <s.Category>[카테고리1]</s.Category>
+                    <s.Title>이승윤 오늘은 디자인을 하다</s.Title>
+                    <s.CreateDate>2021-02-23</s.CreateDate>
+                </s.PostContainer>
+                <s.PostContainer>
+                    <s.Category>[카테고리1]</s.Category>
+                    <s.Title>이승윤 오늘은 디자인을 하다</s.Title>
+                    <s.CreateDate>2021-02-23</s.CreateDate>
+                </s.PostContainer>
+                <s.PageList>
+                    <s.PageNumber>&lt;</s.PageNumber>
+                    <s.PageNumber>1</s.PageNumber>
+                    <s.PageNumber>2</s.PageNumber>
+                    <s.PageNumber>3</s.PageNumber>
+                    <s.PageNumber>4</s.PageNumber>
+                    <s.PageNumber>5</s.PageNumber>
+                    <s.PageNumber>&gt;</s.PageNumber>
+                </s.PageList>
+            </s.MainContainer>
         </>
     );
 }
--- sushi-blog/src/components/MAIN/rightContent/style.js
+++ sushi-blog/src/components/MAIN/rightContent/style.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import {fontColor} from '../../../style/index';
 
-export const PostContainer = styled.div`
+export const MainContainer = styled.div`
     margin-top: 23px;
     background-color: #F6E5D2;
     width: 145vh;
+    height: 870px;
 `
 
 export const ExplanaitionDiv = styled.div`
@@ -13,6 +14,49 @@ export const ExplanaitionDiv = styled.div`
     font-size: 30px;
     font-weight: bold;
     p{
+        margin: 0;
+        margin-top: 30px;
         color:${fontColor};
     }
+`
+
+export const PostContainer= styled.div`
+    cursor: pointer;
+    margin-top: 40px;
+    width: 100%;
+    font-size: 25px;
+    text-align: center;
+    display: flex;
+    justify-content: space-evenly;
+`
+
+export const Category = styled.span`
+    width: 200px;
+    margin-left: 10px;
+    font-weight: bold;
+`
+
+export const Title = styled.span`
+    width: 450px;
+`
+
+export const CreateDate = styled.span`
+    margin-left: -9px;
+    width: 170px;
+`
+
+export const PageList = styled.div`
+    width: 350px;
+    text-align: center;
+    left: 50%;
+    margin-left: -175px;
+    position: absolute;
+    bottom: 5%;
+    margin: auto;
+`
+
+export const PageNumber = styled.span`
+    cursor: pointer;
+    font-size: 20px;
+    margin-right: 25px;
 `
