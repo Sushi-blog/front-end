2f31fab49c224e4f8d9ca72e450de7f357f4cf3a
Kimjimin0807 <jm004471@naver.com>
2021-03-06 23:04:48 +0900

    STYLE: 카테고리 수정 스타일

---
+++ sushi-blog/src/components/MAIN/modals/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const WhiteBox = styled.div`
+    z-index: 1;
+    top: 0%;
+    width: 100%;
+    position: absolute;
+    height: 100%;
+    background-color: #c4c4c4;
+    opacity: 0.5;
+`
+
+export const CategoryFixContainer = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width:1000px;
+    height: 600px;
+    margin-left: -500px;
+    margin-top: -300px;
+    background-color: #FFDFB9;
+    z-index:1;
+`
+
