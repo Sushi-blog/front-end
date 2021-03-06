0ccc434cd606e9e17ee5d3abfcccc662cc83da59
Kimjimin0807 <jm004471@naver.com>
2021-03-07 00:02:07 +0900

    ADD: 카테고리 수정 모달창 퍼블리싱 완료

--- sushi-blog/src/components/MAIN/modals/style.js
+++ sushi-blog/src/components/MAIN/modals/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { fontColor, baseColor } from '../../../style/index';
 
 export const WhiteBox = styled.div`
     z-index: 1;
@@ -10,7 +11,7 @@ export const WhiteBox = styled.div`
     opacity: 0.5;
 `
 
-export const CategoryFixContainer = styled.div`
+export const MainContainer = styled.div`
     position: fixed;
     top: 50%;
     left: 50%;
@@ -22,3 +23,50 @@ export const CategoryFixContainer = styled.div`
     z-index:1;
 `
 
+export const TitleFont = styled.p`
+    margin: 0;
+    font-size: 30px;
+    color: ${fontColor};
+`
+
+export const Font = styled.p`
+    width: 200px;
+    margin: 0;
+    font-size: 30px;
+    color: ${fontColor};
+    float:left;
+`
+
+export const CategoryTitle = styled.div`
+    width: 405px;
+    height:60px;
+    margin: auto;
+    margin-top: 55px;
+    text-align: center;
+    border-bottom: 1px solid;
+`
+
+export const CategoryFixContainer = styled.div`
+    margin-top: 55px;
+    margin-left: 127px;
+    display: flex;
+    align-items: center;
+`
+
+export const CategoryInput = styled.input`
+    width:450px;
+    height: 40px;
+    margin-left: 10px;
+    background-color: ${baseColor};
+    border: none;
+    font-size: 25px;
+    padding: 0;
+`
+
+export const FixBtn = styled.button`
+    margin-left: 25px;
+    width: 55px;
+    height:40px;
+    border: none;
+    background-color: ${baseColor};
+`
