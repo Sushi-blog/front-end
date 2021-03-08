46330d8ea575bb14f85101b0efe0cf03df69b38d
Kimjimin0807 <jm004471@naver.com>
2021-03-08 20:23:30 +0900

    FILE: 파일 위치 수정

--- sushi-blog/src/components/MAIN/modals/categoryFix/style.js
+++ sushi-blog/src/components/MAIN/modals/categoryFix/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { fontColor, baseColor } from '../../../style/index';
+import { fontColor, baseColor } from '../../../../style/index';
 
 export const WhiteBox = styled.div`
     z-index: 1;
