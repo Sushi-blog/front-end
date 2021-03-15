6209b87a480e6f66b9ec29c8f10df32509bf4790
Kimjimin0807 <jm004471@naver.com>
2021-03-15 20:23:16 +0900

    ADD: 로그인 퍼블리싱 완료

--- sushi-blog/src/components/LOGIN/login.js
+++ sushi-blog/src/components/LOGIN/login.js
@@ -5,9 +5,15 @@ const Login = () => {
     return (
         <s.CenterContent>
              <s.LoginContainer>
-                 <s.TitleContainer>
-                     <p>Login</p>
-                 </s.TitleContainer>
+                <s.TitleContainer>
+                    <p>Login</p>
+                </s.TitleContainer>
+                <s.LoginInput placeholder="이메일을 입력히세요"></s.LoginInput>
+                <s.LoginInput placeholder="비밀번호를 입력하세요"></s.LoginInput>
+                <s.CheckBtn>확인</s.CheckBtn>
+                <s.SignUp onClick={()=>{
+                    window.location.href="/signup"
+                }}>회원가입을 하시겠습니까?</s.SignUp>
             </s.LoginContainer>
         </s.CenterContent>
     );
--- sushi-blog/src/components/LOGIN/style.js
+++ sushi-blog/src/components/LOGIN/style.js
@@ -10,7 +10,7 @@ export const LoginContainer = styled.div`
         top:50%;
         transform: translateY(-50%);
         width: 880px;
-        height: 830px;
+        height: 750px;
         background-color: #FFDDB5;
 `
 
@@ -20,4 +20,39 @@ export const TitleContainer = styled.div`
     width: 550px;
     height: 85px;
     border-bottom: 1px solid;
+    margin: auto;
+`
+
+export const LoginInput = styled.input`
+    width: 720px;
+    height: 85px;
+    margin-top: 70px;
+    position: relative;
+    left:50%;
+    transform: translateX(-50%);
+    font-size: 25px;
+    padding-left: 20px;
+`
+
+export const CheckBtn = styled.button`
+    width: 200px;
+    height: 80px;
+    background-color: #8E5454;
+    color: white;
+    font-size: 30px;
+    font-weight: bold;
+    border: none;
+    margin-top: 60px;
+    position:relative;
+    left: 50%;
+    transform:translateX(-50%);
+`
+
+export const SignUp = styled.p`
+    color: #A63B3B;
+    font-size: 20px;
+    text-decoration: underline solid #A63B3B;
+    cursor: pointer;
+    margin-top: 30px;
+    text-align: center;
 `
