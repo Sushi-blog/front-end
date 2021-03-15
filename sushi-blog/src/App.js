d677ea0e12a0969ff9674c92c6be030acdba61a3
Kimjimin0807 <jm004471@naver.com>
2021-03-15 20:23:40 +0900

    CHANGE: 경로수정

--- sushi-blog/src/App.js
+++ sushi-blog/src/App.js
@@ -4,15 +4,17 @@ import GlobalStyle from './style/globalstyle';
 import MainPage from './components/MAIN/main';
 import WritePost from './components/WRITEPOST/writePost';
 import Login from './components/LOGIN/login';
+import SignUp from './components/SIGNUP/signup';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <GlobalStyle></GlobalStyle>
-        <Route exact path="/" component={Login}></Route>
-        <Route exact path="/mainpage" component={MainPage}/>
-        <Route exact path="/write/post" component={WritePost}></Route>
+          <Route exact path="/" component={Login}></Route>
+          <Route exact path="/mainpage" component={MainPage}/>
+          <Route exact path="/write/post" component={WritePost}></Route>
+          <Route exact path="/signup" component={SignUp}></Route>
       </BrowserRouter>
     </>
   );
