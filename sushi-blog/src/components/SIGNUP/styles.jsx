372319659f07a036bb37f1fa6698a0416a840e2f
Kimjimin0807 <jm004471@naver.com>
2021-04-20 17:17:20 +0900

    CHANGE: 회원가입 디자인 수정

--- sushi-blog/src/components/SIGNUP/styles.jsx
+++ sushi-blog/src/components/SIGNUP/styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { loginColor } from '../../style/index';
 
 export const CenterContent = styled.div`
     display:flex;
@@ -9,25 +10,28 @@ export const SignupContainer = styled.div`
     position: absolute;
     top:50%;
     transform: translateY(-50%);
-    width: 880px;
-    height: 800px;
-    background-color: #FFDDB5;
+    width: 700px;
+    height: 750px;
+    background-color: ${loginColor};
+    border-radius: 10px;
 `
 
 export const TitleContainer = styled.p`
     text-align: center;
-    font-size: 50px;
-    width: 550px;
-    height: 85px;
+    font-weight: bold;
+    font-size: 60px;
+    line-height: 59px;
+    width: 435px;
     border-bottom: 1px solid;
+    padding-bottom: 20px;
     margin: auto;
-    margin-top: 25px;
+    margin-top: 58px;
 `
 
 export const SignupInput = styled.input`
-    width: 720px;
-    height: 85px;
-    margin-top: 45px;
+    width: 580px;
+    height: 80px;
+    margin-top: 36px;
     position: relative;
     left:50%;
     transform: translateX(-50%);
@@ -35,17 +39,20 @@ export const SignupInput = styled.input`
     padding-left: 20px;
     border: none;
     border-radius: 10px;
+    &:nth-child(2) {
+        margin-top: 46px;
+    }
 `
 
 export const CheckBtn = styled.button`
-    width: 200px;
-    height: 80px;
-    background-color: #8E5454;
+    width: 150px;
+    height: 73px;
+    background-color: rgba(0, 0, 0, 0.5);
     color: white;
     font-size: 30px;
     font-weight: bold;
     border: none;
-    margin-top: 40px;
+    margin-top: 35px;
     position:relative;
     left: 50%;
     transform:translateX(-50%);
