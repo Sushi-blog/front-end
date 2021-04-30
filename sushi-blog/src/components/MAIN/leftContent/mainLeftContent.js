27b9be32a3dcdc211b3dc872cd389280dffb0963
Kimjimin0807 <jm004471@naver.com>
2021-04-30 17:04:02 +0900

    CHANGE: 코드 수정

--- sushi-blog/src/components/MAIN/leftContent/mainLeftContent.js
+++ sushi-blog/src/components/MAIN/leftContent/mainLeftContent.js
@@ -3,18 +3,26 @@ import * as s from './styles';
 import CategoryFix from '../modals/categoryFix/categoryFix';
 import NicknameFix from '../modals/nicknameFix/nicknameFix';
 import WithdrawalModal from '../modals/withdrawal/withdrawal';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const LeftCotent = () => {
     const [categoryFixModal, setCategoryFixModal] = useState(false);
     const [nicknameFixModal, setNicknameFixModal] = useState(false);
     const [withdrawalModal, setWithdrawalModal] = useState(false);
+    const history = useHistory();
 
     const ViewCategoryFixModal = () => {
         setCategoryFixModal(true);
         return;
     };
 
-    
+    useEffect(() => {
+        if(!localStorage.getItem("token")) {
+            alert("로그인을 해주세요");
+            history.push('/');
+        }
+    }, [])
 
     const ViewNicknameFixModal = () => {
         setNicknameFixModal(true);
@@ -28,9 +36,9 @@ const LeftCotent = () => {
 
     return(
         <>
-            {categoryFixModal ?<CategoryFix setCategoryFixModal={setCategoryFixModal}></CategoryFix>: null}
-            {nicknameFixModal ? <NicknameFix setNicknameFixModal={setNicknameFixModal}></NicknameFix>: null}
-            {withdrawalModal ? <WithdrawalModal setWithdrawalModal={setWithdrawalModal}></WithdrawalModal>: null}
+            {categoryFixModal &&<CategoryFix setCategoryFixModal={setCategoryFixModal}></CategoryFix>}
+            {nicknameFixModal && <NicknameFix setNicknameFixModal={setNicknameFixModal}></NicknameFix>}
+            {withdrawalModal && <WithdrawalModal setWithdrawalModal={setWithdrawalModal}></WithdrawalModal>}
             <s.MainCotainer>
                 <s.ContentCenter>
                     <s.UserName onClick={ViewNicknameFixModal}>김지민<s.HeaderFont>의 블로그</s.HeaderFont></s.UserName>
@@ -43,11 +51,7 @@ const LeftCotent = () => {
                         <s.CategoryMenuFont>카테고리3</s.CategoryMenuFont>
                         <s.CategoryMenuFont>카테고리4</s.CategoryMenuFont>
                     </div>
-                    <s.WritePostBtn 
-                        onClick={() => {
-                            window.location.href = '/write/post';
-                        }}
-                    >게시물 작성</s.WritePostBtn>
+                    <Link to="/write/post"><s.WritePostBtn>게시물 작성</s.WritePostBtn></Link>
                     <s.FixContainer>
                         <s.FixFont onClick={ViewWithdrawalModal}>회원탈퇴</s.FixFont>
                     </s.FixContainer>
